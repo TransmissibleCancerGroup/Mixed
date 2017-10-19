deab5899a1043f9d561db5907b40472d88c43150
Kevin Gori <kcg25@cam.ac.uk>
2017-10-19 18:26:17 +0100

    Updated stats output

--- dice/main.cpp
+++ dice/main.cpp
@@ -1,13 +1,9 @@
 #include <chrono>
 #include <fstream>
 #include <iostream>
-#include <mutex>
 #include <random>
-#include <string>
 #include <thread>
-#include <type_traits>
 #include <unordered_set>
-#include <vector>
 
 #include "pcg/pcg_random.hpp"
 #include "tclap/CmdLine.h"
@@ -19,79 +15,130 @@ struct Mutation_hash {
     }
 };
 
+
+struct RepResult {
+    RepResult(std::vector<int> &totals, std::vector<int> &duplicates) : tot_counts(totals), dup_counts(duplicates) {}
+    std::vector<int> tot_counts;
+    std::vector<int> dup_counts;
+};
+
+
 /* Collects results from threads for use back in sequential-land.
  * Data is protected by a std::mutex */
 struct ResultHelper {
     ResultHelper(int size) {
         result.reserve(size);
     }
-    void handle_result(std::vector<int> thread_result) {
+    void handle_result(std::vector<RepResult> thread_result) {
         std::lock_guard<std::mutex> guard(mtx);
         result.insert(result.end(), thread_result.begin(), thread_result.end());
     }
-    std::vector<int> result;
+    std::vector<RepResult> result;
 private:
     std::mutex mtx;
 };
+
+
+class ProgressBar {
+public:
+    ProgressBar(size_t total) : total(total)  {
+
+    }
+    void update(size_t done) {
+        std::lock_guard<std::mutex> guard(mtx);
+        total_done += done;
+        std::cerr << "\33[2K\r[";
+        int prop_done = ((linewidth - 2) * total_done) / total;
+        for (int i = 0; i < prop_done; i++) {
+            std::cerr << "#";
+        }
+        for (int i = 0; i < (linewidth - 2 - prop_done); i++) {
+            std::cerr << " ";
+        }
+        std::cerr << "] " << total_done << " / " << total << std::flush;
+        //if (total_done >= total) std::cerr << std::endl;
+    }
+private:
+    size_t total;
+    size_t total_done = 0;
+    size_t linewidth = 80;
+    std::mutex mtx;
+};
+
 
 class mutation_generator {
 public:
     mutation_generator(
             unsigned int seed,
             const std::vector<double> &_spectrum,
-            const std::vector<unsigned long> &_opps) : engine { seed }
+            const std::vector<unsigned long> &_opps) : rng { seed }, size { _spectrum.size() }
     {
-        context_gen = std::discrete_distribution<>(_spectrum.begin(), _spectrum.end());
+        if (_spectrum.size() != _opps.size()) {
+            throw std::runtime_error("Spectrum and Opportunities must be same size");
+        }
+        context_generator = std::discrete_distribution<>(_spectrum.begin(), _spectrum.end());
         for (unsigned long s : _opps) {
-            position_gens.push_back(std::uniform_int_distribution<unsigned long>(0, s));
+            position_generators.push_back(std::uniform_int_distribution<unsigned long>(0, s));
         }
     };
 
     mutation_generator(
             pcg_extras::seed_seq_from<std::random_device> &seed_source,
             const std::vector<double> &_spectrum,
-            const std::vector<unsigned long> &_opps) : engine { seed_source }
+            const std::vector<unsigned long> &_opps) : rng { seed_source }, size { _spectrum.size() }
     {
-        context_gen = std::discrete_distribution<>(_spectrum.begin(), _spectrum.end());
+        context_generator = std::discrete_distribution<>(_spectrum.begin(), _spectrum.end());
         for (unsigned long s : _opps) {
-            position_gens.push_back(std::uniform_int_distribution<unsigned long>(0, s));
+            position_generators.push_back(std::uniform_int_distribution<unsigned long>(0, s));
         }
     };
 
     Mutation sample()
     {
-        auto ctxt = context_gen(engine);
-        auto pos = position_gens[ctxt](engine);
+        auto ctxt = context_generator(rng);
+        auto pos = position_generators[ctxt](rng);
         return Mutation{ctxt, pos};
     }
 
-    int run_single_rep(int n_mut) {
-        std::unordered_set<Mutation, Mutation_hash> batch;
+    RepResult run_single_rep(int n_mut) {
+        std::unordered_set<Mutation, Mutation_hash> seen;
+        std::vector<int> dup_counts(size, 0);
+        std::vector<int> tot_counts(size, 0);
+
         for (int i = 0; i < n_mut; i++) {
             Mutation m = sample();
-            if (batch.find(m) != batch.end()) {
-                return 1;
+            tot_counts[m.first]++;
+
+            if (seen.find(m) != seen.end()) {
+                // Duplicate mutation!
+                dup_counts[m.first]++;
             }
+
             else {
-                batch.insert(m);
+                seen.insert(m);
             }
         }
-        return 0;
+
+        return RepResult(tot_counts, dup_counts);
     }
 
-    void run_reps(int n_reps, int n_mut, std::vector<int> &output) {
+    void run_reps(int n_reps, int n_mut, std::vector<RepResult> &output, ProgressBar &pbar) {
         output.reserve(n_reps);
         for (int i = 0; i < n_reps; i++) {
             output.push_back(run_single_rep(n_mut));
+            pbar.update(1);
         }
     }
 
 private:
-    pcg32 engine;
-    std::discrete_distribution<> context_gen;
-    std::vector<std::uniform_int_distribution<unsigned long>> position_gens;
+    pcg32 rng;
+    size_t size;
+    std::discrete_distribution<> context_generator;
+    std::vector<std::uniform_int_distribution<unsigned long>> position_generators;
 };
 
+
+void write_results(std::ofstream &reps_out, const RepResult &rep);
 
 /* Read file line-by-line into vector of strings */
 std::vector<std::string> read_file(const std::string &filename) {
@@ -134,6 +181,15 @@ std::vector<unsigned long> read_opps(const std::string &filename) {
 }
 
 
+void write_csv(std::ofstream &outstream, const std::vector<int> &counts) {
+    for (int i = 0; i < counts.size() - 1; i++) {
+        auto val = counts[i];
+        outstream << val << ",";
+    }
+    outstream << counts.back() << std::endl;
+}
+
+
 int main(int argc, char** argv) {
     auto start = std::chrono::high_resolution_clock::now();
     try {
@@ -148,6 +204,12 @@ int main(int argc, char** argv) {
         TCLAP::UnlabeledValueArg<std::string> oppsArg(
                 "opps",
                 "File containing opps (mutation opportunities), 1 per line",
+                true, "", "string"
+        );
+
+        TCLAP::UnlabeledValueArg<std::string> outpathArg(
+                "outpath",
+                "Directory to write result files (must already exist). Result files will be <path>/repeat_mutations.csv and <path>/total_mutations.csv",
                 true, "", "string"
         );
 
@@ -181,6 +243,7 @@ int main(int argc, char** argv) {
         cmd.add( threadsArg );
         cmd.add( mutationArg );
         cmd.add( seedArg );
+        cmd.add( outpathArg );
 
         // Parse the argv array.
         cmd.parse( argc, argv );
@@ -199,6 +262,27 @@ int main(int argc, char** argv) {
             exit(1);
         }
 
+        // Set up out files
+        std::stringstream reps_filename;
+        reps_filename << outpathArg.getValue() << "/repeat_mutations.csv";
+        std::ofstream reps_out(reps_filename.str());
+        if (!reps_out.is_open()) {
+            std::stringstream ss;
+            ss << "failed to open " << reps_filename.str() << '\n';
+            throw std::runtime_error(ss.str());
+        }
+
+        std::stringstream totals_filename;
+        totals_filename << outpathArg.getValue() << "/total_mutations.csv";
+        std::ofstream totals_out(totals_filename.str());
+        if (!totals_out.is_open()) {
+            std::stringstream ss;
+            ss << "failed to open " << totals_filename.str() << '\n';
+            throw std::runtime_error(ss.str());
+        }
+
+        int ncat = spec.size();
+
         auto nthreads = threadsArg.getValue();
 
         if (nthreads < 1) nthreads = 1;
@@ -207,16 +291,17 @@ int main(int argc, char** argv) {
         auto reps = repsArg.getValue();
         auto n_mut = mutationArg.getValue();
 
-        std::vector<int> result;
-        result.reserve(reps);
+        std::vector<RepResult> result;
+
+        ProgressBar pbar(reps);
 
         if (nthreads == 1) {
             std::cout << "Using 1 thread" << std::endl;
             // Seed RNG and get result (single thread version)
             pcg_extras::seed_seq_from<std::random_device> seed;
-//            unsigned int seed = rd();
             mutation_generator gen(seed, spec, op);
-            gen.run_reps(reps, n_mut, result);
+            result.reserve(reps);
+            gen.run_reps(reps, n_mut, result, pbar);
         }
 
         else {
@@ -235,11 +320,11 @@ int main(int argc, char** argv) {
                 // pcg_extras::seed_seq_from<std::random_device> seed;
                 unsigned int seed = rd();
 
-                threads.push_back(std::thread( [&helper, seed, &spec, &op, reps_per_thread, n_mut]() {
+                threads.push_back(std::thread( [&helper, seed, &spec, &op, &pbar, reps_per_thread, n_mut]() {
                     mutation_generator gen(seed, spec, op);
-                    std::vector<int> res_t;
+                    std::vector<RepResult> res_t;
                     res_t.reserve(reps_per_thread);
-                    gen.run_reps(reps_per_thread, n_mut, res_t);
+                    gen.run_reps(reps_per_thread, n_mut, res_t, pbar);
                     helper.handle_result(res_t);
                 }));
             }
@@ -251,9 +336,70 @@ int main(int argc, char** argv) {
             result = helper.result;
         }
 
-        double sum = std::accumulate(result.begin(), result.end(), 0);
-        std::cout << "Estimated probability of at least one recurrent mutation = "
-                  << sum / result.size() << std::endl;
+        // Print some quick stats
+        std::cout << std::endl;
+        std::cout << "Expected counts of duplicate mutations in each category:" << std::endl;
+        std::cout << "(R equivalent ` colMeans(dups) `)" << std::endl;
+        std::vector<double> counts(ncat, 0.0);
+        for (int i = 0; i < reps; i++) {
+            for (int j = 0; j < ncat; j++) {
+                counts[j] += (double) result[i].dup_counts[j];
+            }
+        }
+        for (auto &val : counts) {
+            val /= reps;
+            std::cout << val << '\t';
+        }
+        std::cout << std::endl;
+
+        std::cout << "Estimated conditional probability of seeing duplicate mutations in each category (conditioned on category):" << std::endl;
+        std::cout << "(R equivalent ` colMeans(dups / totals) `)" << std::endl;
+        std::vector<double> cond_probs(ncat, 0.0);
+        for (int i = 0; i < reps; i++) {
+            for (int j = 0; j < ncat; j++) {
+                if (result[i].tot_counts[j] > 0) {
+                    cond_probs[j] += (double) result[i].dup_counts[j] / (double) result[i].tot_counts[j];
+                }
+            }
+        }
+        for (auto &val : cond_probs) {
+            val /= reps;
+            std::cout << val << '\t';
+        }
+        std::cout << std::endl;
+
+        std::cout << "Estimated overall probability of seeing duplicate mutations in each category:" << std::endl;
+        std::cout << "(R equivalent ` colMeans(dups / sum(totals[1, ])) `)" << std::endl;
+        std::vector<double> probs(ncat, 0.0);
+
+        for (int i = 0; i < reps; i++) {
+            for (int j = 0; j < result[i].dup_counts.size(); j++) {
+                probs[j] += (double) result[i].dup_counts[j] / (double) n_mut;
+            }
+        }
+
+        for (auto &val : probs) {
+            val /= reps;
+            std::cout << val << '\t';
+        }
+        std::cout << std::endl;
+
+        std::cout << "Estimated probability of at least one recurrent mutation:" << std::endl;
+        std::cout << "(R equivalent ` sum(rowSums(dups) > 0) / nrow(dups) `)" << std::endl;
+        int seen = 0;
+        for (auto &rep_result : result) {
+            if (std::accumulate(rep_result.dup_counts.begin(), rep_result.dup_counts.end(), 0) > 0) {
+                seen++;
+            }
+        }
+        std::cout << (double) seen / (double) reps << std::endl;
+
+        for (auto &rep : result) {
+            write_csv(reps_out, rep.dup_counts);
+            write_csv(totals_out, rep.tot_counts);
+        }
+        reps_out.close();
+        totals_out.close();
 
     }
     catch (TCLAP::ArgException &e)  // catch any exceptions
